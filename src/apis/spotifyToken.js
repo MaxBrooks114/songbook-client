316d797d88beb087e1c8db7ead0aec5926d60ab5
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-21 11:04:18 -0400

    spotify token generator

---
+++ src/apis/spotifyToken.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import qs from "qs";
+
+export const getToken = async () => {
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const clientSecret = process.env.REACT_APP_KEY;
+
+  const headers = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    auth: {
+      username: clientId,
+      password: clientSecret,
+    },
+  };
+  const data = {
+    grant_type: "client_credentials",
+  };
+
+  try {
+    const response = await axios.post("https://accounts.spotify.com/api/token", qs.stringify(data), headers);
+    return response.data.access_token;
+  } catch (error) {
+    console.log(error);
+  }
+};
