f56b04334772e66c2cb8f7a6f9a137ded2b65480
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-21 15:50:00 -0400

    spotify search redux refactor

--- src/components/spotify/SpotifySearch.js
+++ src/components/spotify/SpotifySearch.js
@@ -1,36 +1,24 @@
 import React, { Component } from "react";
-import { getToken } from "../../apis/spotifyToken.js";
-import spotify from "../../apis/spotify";
+import { connect } from 'react-redux'
+import { fetchSpotifyTracks } from '../../actions/spotify'
 
 class SpotifySearch extends Component {
-  state = { search: "", token: null, spotifySongs: [] };
+  state = { search: "", token: null };
 
-  async componentDidMount(){
-    const token = await getToken()
-    this.setState({ token: token });
-  }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    spotify.get("/search", {
-      headers: {
-        'Authorization': `Bearer ${this.state.token}`,
-        'Accept': "application/json",
-        "Content-Type": "application/json",
-      },
-      params: {
-        q: this.state.search,
-        type: "track",
-      },
-    });
+    this.props.fetchSpotifyTracks(this.state.search)
 
-    
   };
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
+          <label>
+            <h1>Spotify Search</h1>
+          </label><br />
           <input type="text" value={this.state.search} onChange={(e) => this.setState({ search: e.target.value })} />
           <button>Submit</button>
         </form>
@@ -39,4 +27,6 @@ class SpotifySearch extends Component {
   }
 }
 
-export default SpotifySearch;
+
+
+export default connect(null, {fetchSpotifyTracks})(SpotifySearch);
