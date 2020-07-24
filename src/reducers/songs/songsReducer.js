a87ef0a537a8c82bf85a30c20dbf9d5557a3f864
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-23 20:05:34 -0400

    songs reducer

---
+++ src/reducers/songs/songsReducer.js
@@ -0,0 +1,19 @@
+import { CREATE_SONG, FETCH_SONGS, FETCH_SONG, EDIT_SONG, DELETE_SONG } from "../../actions/songs/types";
+import _ from "lodash";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_SONGS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case FETCH_SONG:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_SONG:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_SONG:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_SONG:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
