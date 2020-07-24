68e2b6617ee60a5dc1948e65a27c74c74e55b4c4
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-23 20:05:26 -0400

    add form and song reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
-import {combineReducers} from 'redux'
-import spotifyTracksReducer from './spotify/spotifyTracksReducer'
-
+import { combineReducers } from "redux";
+import spotifyTracksReducer from "./spotify/spotifyTracksReducer";
+import songsReducer from "./songs/songsReducer";
+import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
-    spotifyTracks: spotifyTracksReducer
-})
+  spotifyTracks: spotifyTracksReducer,
+  form: formReducer,
+  songs: songsReducer,
+});
