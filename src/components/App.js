eb86723ce93c3041aa1d649de3e3d85b11334dbb
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-23 20:03:58 -0400

    navigation and component imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 import SpotifySearch from "./spotify/SpotifySearch";
+import SongForm from "./songs/SongForm";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
+import history from "../history";
+import { MuiThemeProvider } from "material-ui/styles";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>
-        <SpotifySearch />
-      </div>
+      <Router history={history}>
+        <div>
+          <Switch>
+            <Route path="/search" exact component={SpotifySearch} />
+            <MuiThemeProvider>
+              <Route path="/songs/new" exact component={SongForm} />
+            </MuiThemeProvider>
+          </Switch>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 };
