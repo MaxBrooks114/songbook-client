471d874f146e06f55f3dca86cf0fb9296dc118ec
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-23 20:04:21 -0400

    array of musical keys

---
+++ src/components/songs/keys.js
@@ -0,0 +1,16 @@
+const keys = [
+  { name: "C" },
+  { name: "C#" },
+  { name: "D" },
+  { name: "D#" },
+  { name: "E" },
+  { name: "F" },
+  { name: "F#" },
+  { name: "G" },
+  { name: "G#" },
+  { name: "A" },
+  { name: "A#" },
+  { name: "B" },
+];
+
+export default keys;
