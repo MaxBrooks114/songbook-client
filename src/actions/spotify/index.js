6dd2d0e9c938f72d34ab536fdfe04052ec505b22
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-25 14:09:18 -0400

    import spotify track action triggers create song

--- src/actions/spotify/index.js
+++ src/actions/spotify/index.js
@@ -1,6 +1,7 @@
 import spotify from "../../apis/spotify";
 import { getToken } from "../../apis/spotifyToken";
 import { FETCH_SPOTIFY_TRACKS, CLEAR_SPOTIFY_TRACKS } from "./types";
+import { createSong } from "../songs/index";
 
 export const fetchSpotifyTracks = (query) => async (dispatch) => {
   const token = await getToken();
@@ -30,4 +31,61 @@ export const clearSpotifyTracks = () => {
   return {
     type: CLEAR_SPOTIFY_TRACKS,
   };
+};
+
+export const importSpotifyTrack = (id) => async (dispatch) => {
+  const token = await getToken();
+
+  const trackData = await spotify.get(`/tracks/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  const audioFeatureData = await spotify.get(`/audio-features/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  const artistData = await spotify.get(`/artists/${trackData.data.artists[0].id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  const songData = {
+    title: trackData.data.name,
+    artist: trackData.data.artists[0].name,
+    album: trackData.data.album.name,
+    year: trackData.data.album.release_date,
+    image: trackData.data.album.images[1].url,
+    genre: artistData.data.genres[0],
+    duration: trackData.data.duration_ms,
+    explicit: trackData.data.explicit,
+    key: audioFeatureData.data["key"],
+    mode: audioFeatureData.data["mode"],
+    lyrics: "",
+    time_signature: audioFeatureData.data["time_signature"],
+    tempo: audioFeatureData.data["tempo"],
+    acousticness: audioFeatureData.data["acousticness"],
+    danceability: audioFeatureData.data["danceability"],
+    energy: audioFeatureData.data["energy"],
+    instrumentalness: audioFeatureData.data["instrumentalness"],
+    liveness: audioFeatureData.data["liveness"],
+    loudness: audioFeatureData.data["loudness"],
+    speechiness: audioFeatureData.data["speechiness"],
+    valence: audioFeatureData.data["valence"],
+    original: false,
+    spotify_url: trackData.data.uri,
+    spotify_id: trackData.data.id,
+  };
+
+  dispatch(createSong(songData));
 };
