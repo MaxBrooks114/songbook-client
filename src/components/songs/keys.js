38663d5cd0ddadab1da81dfa69280fcf721f6d68
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-25 14:05:56 -0400

    rename key object attribute with number

--- src/components/songs/keys.js
+++ src/components/songs/keys.js
@@ -1,16 +1,16 @@
 const keys = [
-  { name: "C" },
-  { name: "C#" },
-  { name: "D" },
-  { name: "D#" },
-  { name: "E" },
-  { name: "F" },
-  { name: "F#" },
-  { name: "G" },
-  { name: "G#" },
-  { name: "A" },
-  { name: "A#" },
-  { name: "B" },
+  { 0: "C" },
+  { 1: "C#" },
+  { 2: "D" },
+  { 3: "D#" },
+  { 4: "E" },
+  { 5: "F" },
+  { 6: "F#" },
+  { 7: "G" },
+  { 8: "G#" },
+  { 9: "A" },
+  { 10: "A#" },
+  { 11: "B" },
 ];
 
 export default keys;
