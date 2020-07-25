0b5fc0b58b60c80daa2b036e7f389b54260bf33c
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-07-25 14:02:49 -0400

    import spotify track reducer

--- src/reducers/spotify/spotifyTracksReducer.js
+++ src/reducers/spotify/spotifyTracksReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_SPOTIFY_TRACKS, CLEAR_SPOTIFY_TRACKS } from "../../actions/spotify/types";
+import { FETCH_SPOTIFY_TRACKS, CLEAR_SPOTIFY_TRACKS, IMPORT_SPOTIFY_TRACK } from "../../actions/spotify/types";
 import _ from "lodash";
 
 const initialState = {};
@@ -8,9 +8,10 @@ export default (state = initialState, action) => {
     case FETCH_SPOTIFY_TRACKS:
       return { ...state, ..._.mapKeys(action.payload, "name") };
     case CLEAR_SPOTIFY_TRACKS:
-      return initialState
+      return initialState;
+    case IMPORT_SPOTIFY_TRACK:
+      return { ...state, [action.payload.id]: action.payload };
     default:
       return state;
   }
 };
-
