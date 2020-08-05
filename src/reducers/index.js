c83bbd15f0fabf102631f0dd7e655f2c1c4eac83
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-05 16:15:33 -0400

    import filter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
-import { combineReducers } from "redux";
-import spotifyTracksReducer from "./spotify/spotifyTracksReducer";
-import songsReducer from "./songs/songsReducer";
-import uiReducer from "./uiReducer";
-import { reducer as formReducer } from "redux-form";
+import { combineReducers } from 'redux';
+import spotifyTracksReducer from './spotify/spotifyTracksReducer';
+import songsReducer from './songs/songsReducer';
+import uiReducer from './uiReducer';
+import filterReducer from './filterReducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
   spotifyTracks: spotifyTracksReducer,
   form: formReducer,
   songs: songsReducer,
   ui: uiReducer,
+  filter: filterReducer,
 });
