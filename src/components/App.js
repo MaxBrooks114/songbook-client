53bfefac714957bad5e24037475b14274c693653
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-06 12:52:24 -0400

    fixed import statements

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import SpotifySearch from "./spotify/SpotifySearch";
-import SongCreate from "./songs/SongCreate";
-import SongList from "./songs/SongList";
-import SongEdit from "./songs/SongEdit";
-import SuccessSnackBar from "./ui/SuccessSnackBar";
-import Navbar from "./Navbar";
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "../theme";
-import history from "../history";
-import { MuiThemeProvider } from "material-ui/styles";
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import SpotifySearch from './spotify/SpotifySearch';
+import SongCreate from './songs/SongCreate';
+import SongList from './songs/SongList';
+import SongEdit from './songs/SongEdit';
+import SuccessSnackBar from './ui/SuccessSnackBar';
+import Navbar from './ui/Navbar';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './ui/theme';
+import history from '../history';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 const App = () => {
   return (
