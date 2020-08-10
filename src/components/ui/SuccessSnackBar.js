103f80e11022ba5d527486131208b0d6a647e874
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-10 09:31:33 -0400

    style snackbar for song imports

--- src/components/ui/SuccessSnackBar.js
+++ src/components/ui/SuccessSnackBar.js
@@ -1,27 +1,25 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Snackbar from "@material-ui/core/Snackbar";
-import IconButton from "@material-ui/core/IconButton";
-import { Icon } from "@material-ui/core";
-import { clearSnackbar } from "../../actions/ui";
-import { makeStyles } from "@material-ui/styles";
-import MuiAlert from "@material-ui/lab/Alert";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import { Icon } from '@material-ui/core';
+import { clearSnackbar } from '../../actions/ui';
+import { makeStyles } from '@material-ui/styles';
+import MuiAlert from '@material-ui/lab/Alert';
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: theme.palette.common.navy,
+    textTransform: 'capitalize',
+    color: theme.palette.secondary.main,
+    position: 'fixed',
+  },
+}));
 
 export default function SuccessSnackbar() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      background: "rgba(86,3,114,1)",
-      textTransform: "capitalize",
-      color: theme.palette.primary.main,
-      position: "fixed",
-      width: "100%",
-    },
-  }));
-
   const classes = useStyles();
   const dispatch = useDispatch();
 
@@ -34,15 +32,15 @@ export default function SuccessSnackbar() {
   return (
     <Snackbar
       anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "left",
+        vertical: 'bottom',
+        horizontal: 'left',
       }}
       open={successSnackbarOpen}
       autoHideDuration={4000}
       onClose={handleClose}
       aria-describedby="client-snackbar"
     >
-      <Alert onClose={handleClose} severity="success">
+      <Alert onClose={handleClose} className={classes.root} severity="success">
         {successSnackbarMessage}
       </Alert>
     </Snackbar>
