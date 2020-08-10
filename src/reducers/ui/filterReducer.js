13e34ab0658c10bf8e2d881f74cfe3e1df570010
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-10 09:30:19 -0400

    store filter in state

---
+++ src/reducers/ui/filterReducer.js
@@ -0,0 +1,17 @@
+import { SET_FILTER, CLEAR_FILTER } from '../../actions/filter/types';
+
+const initialState = {
+  attribute: '',
+  value: '',
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_FILTER:
+      return { ...state, ...action.payload };
+    case CLEAR_FILTER:
+      return initialState;
+    default:
+      return state;
+  }
+};
