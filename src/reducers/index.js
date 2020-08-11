cf96cd94b96e987dafd44b67cca1ff0a18f7336e
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-11 09:52:37 -0400

    import loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@ import songsReducer from './songs/songsReducer';
 import snackbarReducer from './ui/snackbarReducer';
 import filterReducer from './ui/filterReducer';
 import { reducer as formReducer } from 'redux-form';
+import loadingReducer from './ui/loadingReducer';
 
 export default combineReducers({
   spotifyTracks: spotifyTracksReducer,
   form: formReducer,
   songs: songsReducer,
   snackbar: snackbarReducer,
+  loading: loadingReducer,
   filter: filterReducer,
 });
