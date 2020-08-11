91d198059541221605b5cd2fe9ca4101cb4ff09d
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-11 16:00:57 -0400

    import instrument reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import spotifyTracksReducer from './spotify/spotifyTracksReducer';
 import songsReducer from './songs/songsReducer';
+import instrumentsReducer from './instruments/instrumentsReducer';
 import snackbarReducer from './ui/snackbarReducer';
 import filterReducer from './ui/filterReducer';
 import { reducer as formReducer } from 'redux-form';
@@ -10,6 +11,7 @@ export default combineReducers({
   spotifyTracks: spotifyTracksReducer,
   form: formReducer,
   songs: songsReducer,
+  instruments: instrumentsReducer,
   snackbar: snackbarReducer,
   loading: loadingReducer,
   filter: filterReducer,
