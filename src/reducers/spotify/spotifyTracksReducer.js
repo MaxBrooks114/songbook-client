fe252dfb55db46524689c98d48362f05a930af37
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-14 09:28:12 -0400

    single quotes instead of double quotes

--- src/reducers/spotify/spotifyTracksReducer.js
+++ src/reducers/spotify/spotifyTracksReducer.js
@@ -1,12 +1,12 @@
-import { FETCH_SPOTIFY_TRACKS, CLEAR_SPOTIFY_TRACKS, IMPORT_SPOTIFY_TRACK } from "../../actions/spotify/types";
-import _ from "lodash";
+import { FETCH_SPOTIFY_TRACKS, CLEAR_SPOTIFY_TRACKS, IMPORT_SPOTIFY_TRACK } from '../../actions/types';
+import _ from 'lodash';
 
 const initialState = {};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_SPOTIFY_TRACKS:
-      return { ...state, ..._.mapKeys(action.payload, "name") };
+      return { ...state, ..._.mapKeys(action.payload, 'name') };
     case CLEAR_SPOTIFY_TRACKS:
       return initialState;
     case IMPORT_SPOTIFY_TRACK:
