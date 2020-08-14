161e9365974031130f9c1865c6628857a0aa8fcc
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-14 09:41:22 -0400

    changed import location

--- src/components/spotify/SpotifySearch.js
+++ src/components/spotify/SpotifySearch.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, connect } from 'react-redux';
 import SpotifySearchBar from './SpotifySearchBar';
 import SpotifyTrackList from './SpotifyTrackList';
-import { fetchSpotifyTracks } from '../../actions/spotify/index';
+import { fetchSpotifyTracks } from '../../actions/spotify';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
