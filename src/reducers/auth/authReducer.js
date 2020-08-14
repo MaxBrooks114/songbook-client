f7d3b6803a8f97394459605d8f676e4e25bad399
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-14 09:31:14 -0400

    authentication reducer

---
+++ src/reducers/auth/authReducer.js
@@ -0,0 +1,59 @@
+import {
+  FETCH_USER,
+  LOGIN_USER,
+  LOGIN_FAIL,
+  AUTH_ERROR,
+  LOGOUT_USER,
+  USER_LOADING,
+  REGISTER_USER,
+  REGISTER_FAIL,
+} from '../../actions/types';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: false,
+  user: null,
+  isLoading: false,
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case FETCH_USER:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+        isLoading: false,
+      };
+    case REGISTER_USER:
+    case LOGIN_USER:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+      };
+    case LOGOUT_USER:
+    case AUTH_ERROR:
+    case REGISTER_FAIL:
+    case LOGIN_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+      };
+
+    default:
+      return state;
+  }
+}
