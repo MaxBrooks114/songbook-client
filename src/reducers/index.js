01edb5cf3ef680f19d6a70602544f93b62987690
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-14 09:32:09 -0400

    import auth, messages and error reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,8 +6,14 @@ import snackbarReducer from './ui/snackbarReducer';
 import filterReducer from './ui/filterReducer';
 import { reducer as formReducer } from 'redux-form';
 import loadingReducer from './ui/loadingReducer';
+import authReducer from './auth/authReducer';
+import messagesReducer from './messagesReducer';
+import errorsReducer from './errorsReducer';
 
 export default combineReducers({
+  auth: authReducer,
+  errors: errorsReducer,
+  messages: messagesReducer,
   spotifyTracks: spotifyTracksReducer,
   form: formReducer,
   songs: songsReducer,
