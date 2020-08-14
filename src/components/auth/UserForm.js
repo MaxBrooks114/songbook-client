04043e5ff63f59019613c0b5f0768076b5972eee
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-14 12:10:20 -0400

    remove unused imports

--- src/components/auth/UserForm.js
+++ src/components/auth/UserForm.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
 import { useDispatch, useSelector } from 'react-redux';
 import { createMessage } from '../../actions/messages';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
