2dee938ba09ee3802438852920ae452c9ff6b3a6
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-08-31 17:08:12 -0400

    render login with spotify if no access token

--- src/components/auth/UserShow.js
+++ src/components/auth/UserShow.js
@@ -23,17 +23,21 @@ const useStyles = makeStyles((theme) => ({
 const UserShow = ({ match }) => {
   const classes = useStyles();
   const user = useSelector((state) => state.auth.user);
+  const accessToken = useSelector((state) => state.spotifyUser.accessToken);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(setToken(match.params.accessToken));
-    console.log(match.params.accessToken);
   }, [dispatch, match.params.accessToken]);
+
+  const spotifyLoginButton = () => {
+    return accessToken ? '' : <a href="http://localhost:8000/spotify/login">Integrate with Spotify</a>;
+  };
 
   return (
     <div>
       <h1>{user.username}'s Songbook</h1>
-      <a href="http://localhost:8000/spotify/login">Integrate with Spotify</a>
+      {spotifyLoginButton()}
     </div>
   );
 };
