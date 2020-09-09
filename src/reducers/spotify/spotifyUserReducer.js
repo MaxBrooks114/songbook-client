bdff25bdb5a319301245d0f91e9d69aca0bc0791
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-09-09 13:41:56 -0400

    set, get and refresh spotify access tokens, and device id

--- src/reducers/spotify/spotifyUserReducer.js
+++ src/reducers/spotify/spotifyUserReducer.js
@@ -8,12 +8,37 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case SET_TOKEN:
-      localStorage.setItem('accessToken', action.payload);
+    case SET_TOKENS:
+      localStorage.setItem('accessToken', action.payload.accessToken);
+      localStorage.setItem('refreshToken', action.payload.refreshToken);
       return {
         ...state,
       };
 
+    case GET_TOKENS:
+      return {
+        ...state,
+        accessToken: localStorage.getItem('accessToken'),
+        refreshToken: localStorage.getItem('refreshToken'),
+      };
+
+    case REFRESH_ACCESS_TOKEN:
+      localStorage.setItem('accessToken', action.payload);
+      return {
+        ...state,
+        accessToken: action.payload,
+      };
+    case GET_DEVICE_ID:
+      return {
+        ...state,
+        deviceId: action.payload,
+      };
+    case CLEAR_ALL:
+      return {
+        deviceId: null,
+        accessToken: null,
+        refreshToken: null,
+      };
     default:
       return state;
   }
