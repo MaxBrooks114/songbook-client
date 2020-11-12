2d017b603768d5cfd7a8a8cc9cf672e5dfb016fc
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-11-12 11:40:16 -0500

    added user metrics to user show page

--- src/components/auth/UserShow.js
+++ src/components/auth/UserShow.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
+import {favorite, favoriteInstrument, elementsLearned, attrPreference, minAttr, maxAttr} from '../../helpers/userMetrics'
+import {renderText, millisToMinutesAndSeconds, sec2time} from '../../helpers/detailHelpers'
+import modes from '../songs/modes';
+import keys from '../songs/keys';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,23 +30,140 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const UserShow = ({ match }) => {
+const UserShow = () => {
   const classes = useStyles();
   const user = useSelector((state) => state.auth.user);
   const accessToken = useSelector((state) => state.auth.user.spotify_info.access_token);
+  const songs = useSelector(state => state.songs)
+  const elements = useSelector(state => state.elements)
+  const instruments = useSelector(state => state.instruments)
+  let metrics = [];
+  if (Object.values(songs).length && Object.values(instruments).length && Object.values(elements).length) {
+    metrics = [{
+      title: "Songs",
+      data: Object.values(songs).length
+    }, {
+      title: "Instruments",
+      data: Object.values(instruments).length
+    },
+    {
+      title: "Elements",
+      data: Object.values(elements).length},
+    {
+      title: "Elements Learned",
+      data: elementsLearned(Object.values(elements))
+    },
+    {
+      title: "Favorite Instrument",
+      data: favoriteInstrument(Object.values(elements), Object.values(instruments))
+    },
+    {
+      title: "Favorite Genre",
+      data: favorite(Object.values(songs), 'genre')
+    },
+    {
+      title: "Favorite Key",
+      data: renderText(keys, favorite(Object.values(elements), 'key'))
+    },
+    {
+      title: "Favorite Mode",
+      data: renderText(modes, favorite(Object.values(elements), 'mode'))
+    },
+    {
+      title: "Favorite Artist",
+      data: favorite(Object.values(songs), 'artist')
+    },
+    {
+      title: "Valence Preference",
+      data: attrPreference(Object.values(songs), 'valence')
+    },
+    {
+      title: "Instrumentalness Preference",
+      data: attrPreference(Object.values(songs), 'instrumentalness')
+    },
+    {
+      title: "Energy Preference",
+      data: attrPreference(Object.values(songs), 'energy')
+    },
+    {
+      title: "Speechiness Preference",
+      data: attrPreference(Object.values(songs), 'speechiness')
+    },
+    {
+      title: "Liveness Preference",
+      data: attrPreference(Object.values(songs), 'liveness')
+    },
+    {
+      title: "Shortest Song",
+      data: `${minAttr(Object.values(songs), 'duration').title} (${millisToMinutesAndSeconds(minAttr(Object.values(songs), 'duration').duration)})`
+    },
+    {
+      title: "Shortest Element",
+      data: `${minAttr(Object.values(elements), 'duration').name} of ${minAttr(Object.values(elements), 'duration').song.title} (${sec2time(minAttr(Object.values(elements), 'duration').duration)})`
+    }, {
+      title: "Longest Song",
+      data: `${maxAttr(Object.values(songs), 'duration').title} (${millisToMinutesAndSeconds(maxAttr(Object.values(songs), 'duration').duration)})`
+    },
+    {
+      title: "Longest Element",
+      data: `${maxAttr(Object.values(elements), 'duration').name} of ${maxAttr(Object.values(elements), 'duration').song.title} (${ sec2time(maxAttr(Object.values(elements), 'duration').duration)})`
+    },
+    {
+      title: "Fastest Song",
+      data: `${maxAttr(Object.values(songs), 'tempo').title} (${(maxAttr(Object.values(songs), 'tempo').tempo)}) BPM`
+    },
+    {
+      title: "Fastest Element",
+      data: `${maxAttr(Object.values(elements), 'tempo').name} of ${maxAttr(Object.values(elements), 'tempo').song.title} (${(maxAttr(Object.values(elements), 'tempo').tempo)}) BPM`
+    },
+    {
+      title: "Slowest Song",
+      data: `${minAttr(Object.values(songs), 'tempo').title} (${(minAttr(Object.values(songs), 'tempo').tempo)}) BPM`
+    },
+    {
+      title: "Slowest Element",
+      data: `${minAttr(Object.values(elements), 'tempo').name} of ${minAttr(Object.values(elements), 'tempo').song.title} (${(minAttr(Object.values(elements), 'tempo').tempo)}) BPM`
+    },
+
+   ]
+  }
+  
 
   const spotifyLoginButton = () => {
-    return accessToken && accessToken !== 'undefined' ? (
+    return accessToken && accessToken !== '' ? (
       ''
     ) : (
-      <a href="http://localhost:8000/api/spotify/login">Integrate with Spotify</a>
+      <a href={`http://localhost:8000/api/spotify/login/${user.id}`}>Integrate with Spotify</a>
     );
   };
 
   return (
-    <div>
+    <div className={classes.root}>
       <h1>{user.username}'s Songbook</h1>
+      <Grid
+        container
+        spacing={4}
+        >
+        {metrics.map((metric, index) => {
+         return (
+           <Grid
+             key={metric.title + index}
+             item
+             component={Card}
+             lg={3}
+             sm={6}
+             xl={3}
+             xs={12}
+           >
+            {metric.title} <br/>
+            {metric.data}
+           </Grid>)
+           })}
+      </Grid>
       {spotifyLoginButton()}
+      <Link className={classes.link} to={`${user.id}/edit`}>
+          <Button className={classes.button}>Edit </Button>
+      </Link>
     </div>
   );
 };
---
+++ src/helpers/userMetrics.js
@@ -0,0 +1,61 @@
+import _ from 'lodash'
+
+export const favorite = (list, attr) => {
+  return _.maxBy(Object.values(_.groupBy(list, el => el[attr])), arr => arr.length)[0][attr];
+}
+
+export const favoriteInstrument = (elements, instruments) => {
+  const instrumentMap = elements.filter((element) => {
+      if (element.instruments.length) {
+        return element.instruments
+      }
+    }).map((element) => element.instruments).flat()
+  const instrumentId = _.head(_(instrumentMap)
+  .countBy()
+  .entries()
+  .maxBy(_.last));
+ const fav_instrument = instruments.find(instrument => instrument.id === parseInt(instrumentId))
+
+ return fav_instrument.name
+}
+
+
+export const elementsLearned = (elements) => {
+  return elements.filter(element => element.learned).length
+}
+
+export const attrPreference = (songs, attr) => {
+
+  const low = []
+  const medium = []
+  const high = []
+  for (let song of songs) {
+    if(song[attr] <= 0.35){
+      low.push(song[attr])
+    } else if (song[attr] > 0.35 && song[attr] <= 0.7) {
+      medium.push(song[attr])
+    } else if  (song[attr]> 0.7) {
+      high.push(song[attr])
+    } else {
+      continue
+    }
+  }
+  if (Math.max(low.length, medium.length, high.length) === low.length) return 'low'
+  else if (Math.max(low.length, medium.length, high.length) === medium.length) return 'medium'
+  else return 'high'
+
+ }
+
+ export const maxAttr = (songs, feature) => {
+  return songs.reduce((max, song) => max[feature] > song[feature] ? max : song)
+
+ }
+
+ export const minAttr = (songs, feature) => {
+  return songs.reduce((min, song) => min[feature] < song[feature] ? min : song)
+
+ }
+
+
+
+
