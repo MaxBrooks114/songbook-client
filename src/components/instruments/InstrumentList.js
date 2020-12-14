c7ab1d3fe38165e35e45ba13f54e309aaa7287b7
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-12-14 12:24:06 -0500

    style list and responsiveness

--- src/components/instruments/InstrumentList.js
+++ src/components/instruments/InstrumentList.js
@@ -1,34 +1,83 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchInstruments, fetchInstrument } from '../../actions/instruments';
 import {checkIfPlaying} from '../../actions/spotify'
 import * as workerTimers from 'worker-timers';
-
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/styles';
 import InstrumentCard from './InstrumentCard';
 import InstrumentDetail from './InstrumentDetail';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowUpRoundedIcon from '@material-ui/icons/KeyboardArrowUpRounded';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import useHeight from '../../hooks/useHeight'
+
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
-    height: 640,
-    paddingBottom: theme.spacing(4),
+   
+    paddingBottom: theme.spacing(8),
   },
 
   list: {
-    height: '100%',
+    minHeight: '100vh',
     overflow: 'scroll',
   },
 
-  filter: {
-    marginRight: 14,
-    marginLeft: 14,
-    background: theme.palette.primary.light,
-    borderRadius: '0 0 8px 8px',
+   toolbarMargin: {
+    ...theme.mixins.toolbar,
+    [theme.breakpoints.down('md')]: {
+      marginBottom: '.7rem',
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '1rem',
+    },
   },
+
+  
+    detail: {
+     height: '100%',
+  },
+
+  drawer: {
+    background: theme.palette.primary.dark,
+    color: theme.palette.secondary.main,
+    width: '83%',
+    margin: 'auto',
+    marginTop: theme.spacing(6),
+    height: '50%',
+    [theme.breakpoints.down('xs')]: {
+      height: '40%',
+      
+    },
+   }, 
+
+   drawerIconContainer: {
+    backgroundColor: theme.palette.secondary.main,
+    height: '24px',
+    width: '24px',
+    marginLeft: 0,
+    position: 'fixed',
+    top: '50%',
+    right: 0,
+  
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+    },
+  },
+
+  drawerIcon: {
+    height: '50px',
+    width: '50px',
+  },
+
+
 }));
+
 
 const InstrumentList = ({ match }) => {
   const instruments = useSelector((state) => state.instruments);
@@ -39,6 +88,14 @@ const InstrumentList = ({ match }) => {
   const dispatch = useDispatch();
 
   const classes = useStyles();
+
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const elementDOM = useRef(null);
+  const [height] = useHeight(elementDOM);
+
   let transitionDuration = 50;
 
   const handleClick = (id) => {
@@ -77,17 +134,42 @@ const InstrumentList = ({ match }) => {
     return instrument ? <InstrumentDetail instrument={instrument} /> : null;
   };
 
-  return (
-    <Grid container className={classes.cardGrid}>
-      <Grid item xs={2} className={classes.list}>
+
+  const drawer = (
+    <>
+      <IconButton  className={classes.drawerIconContainer}>
+        <KeyboardArrowUpRoundedIcon onClick={() => setOpenDrawer(!openDrawer)} className={classes.drawerIcon} />
+      </IconButton>
+      
+      <SwipeableDrawer
+        classes={{ paper: classes.drawer }}
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={openDrawer}
+        variant="persistent"
+        anchor="bottom"
+        onClose={() => setOpenDrawer(false)}
+        onOpen={() => setOpenDrawer(true)}
+      >
         <List>{renderedList}</List>
-      </Grid>
-      <Grid item xs={3}></Grid>
-      <Grid item xs={4} className={classes.list}>
-        {renderDetail()}
-      </Grid>
-      <Grid item xs={1}></Grid>
-    </Grid>
+      </SwipeableDrawer>
+    </>
+  );
+
+
+  return (
+     <div >
+        <Grid container justify='center' className={classes.cardGrid}>
+        {!matches ? <Grid item xs={3} md={3}  lg={3}  className={classes.list}>
+            <List style={{height: height}}>{renderedList}</List>  
+        </Grid> : drawer}
+        <Grid item lg={1} md={1} sm={0} xs={0}/>
+        <Grid item xs={10} md={8} lg={6}  ref={elementDOM} className={classes.detail}>
+          {renderDetail()}
+        </Grid>
+    
+        </Grid>
+    </div>
   );
 };
 
