4194ffe24831026f5ced5fb7ef39c5f946c91c50
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-12-14 12:27:46 -0500

    remove unused typography prop

--- src/components/elements/ElementCard.js
+++ src/components/elements/ElementCard.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { useSelector } from 'react-redux';
 import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Slide from '@material-ui/core/Slide';
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,13 +13,14 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: 'auto',
     boxShadow: theme.shadows[24],
+    '&:hover': {
+      cursor: 'pointer',
+    },
      [theme.breakpoints.down('sm')]: {
         textAlign: 'center'
     
       },
-    '&:hover': {
-      cursor: 'pointer',
-    },
+    
     color: '#FFF',
 
     
@@ -42,14 +40,13 @@ const useStyles = makeStyles((theme) => ({
 
 const ElementCard = ({ element, transitionDuration, handleClick }) => {
   const classes = useStyles();
-  const theme = useTheme()
 
   return (
     <Slide direction="up" mountOnEnter in timeout={transitionDuration}>
       <Card className={classes.root} onClick={() => handleClick(element.id)}>
         <CardContent className={classes.cardContent}>
-          <Typography display="block" variant="subtitle1">{element.name}</Typography>
-          <Typography display="block" variant="caption">{element.song.title}</Typography>
+          <Typography  variant="subtitle1">{element.name}</Typography>
+          <Typography  variant="caption">{element.song.title}</Typography>
         </CardContent>
       </Card>
     </Slide>
