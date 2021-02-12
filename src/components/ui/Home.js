b8d79ae5c0b0152c0eee2eab795929af4fccd453
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2021-02-12 10:57:25 -0500

    edit copy

--- src/components/ui/Home.js
+++ src/components/ui/Home.js
@@ -106,31 +106,31 @@ const Home = () => {
     {
       title: 'Songs',
       subtitle: 'Add your own or Import from Spotify',
-      desc: 'Use a form to add a song. This way of adding songs is most useful for your own original music. You can add additional details for your song including key, mode, tempo, lyrics, and your own album artwork! Alternatively, you can import any song from Spotify by typing a query into the search bar above. Importing a song  will give you access to specific metrics from Spotify’s own database, which in turn can be used to discover trends in the music you like, but more on that later. Another great feature of importing from Spotify is that the song is automatically segmented into sections for you.',
+      desc: 'Add a song using using the quick and easy submission form. This way of adding songs is most useful for your original music. You can add additional details for your song including key, mode, tempo, lyrics, and your own album artwork! Alternatively, you can import any song from Spotify by typing a query into the search bar above. Importing a song  will give you access to specific metrics from Spotify’s own database, which are then used to discover trends in the music you like, but more on that later. Another great feature of importing from Spotify is that the song is automatically segmented into sections for you.',
       video: SongsDemo
     },
     {
       title: 'Sections',
       subtitle: 'The building blocks of music',
-      desc: 'In music, a section is defined as a complete, but not independent, musical idea. Examples of sections include verse, chorus, bridge, solo etc. This idea is explored in Songbook in the way that songs contain various sections that you as a musician either write, or learn. For every song you add, you can add any number of sections and information on that section such as the key, mode, tempo, start, duration, lyrics, etc.. Furthermore Songbook gives you access to tools such as a metronome, an audio recorder, the ability to upload sheet music/ tabs, and more!',
+      desc: 'In music, a section is defined as a complete, but not independent, musical idea. Examples of sections include verse, chorus, bridge, solo etc. SongBook helps to organize and catalogue those various sections that you as a musician either write, or learn. For every song you add, you can add any number of sections and the musical properties for that section such as the key, mode, tempo, start, duration, lyrics, etc.. Songbook also gives you access to useful tools such as a metronome, an audio recorder, the ability to upload sheet music and/or tabs, and more!',
       video: SectionDemo
     },
     {
       title: 'Instruments',
       subtitle: 'Tools of the Trade',
-      desc: 'A more optional feature of SongBook, you can add any instrument you can think of and link it to any section you want. Perfect for multi-instrumentalists who know a section or song on more than one instrument. When viewing any particular instrument, you can see a list of all songs and sections you know on that instrument.',
+      desc: 'As a more optional feature of SongBook, you can add any instrument imaginable and link it to any section you want. This feature is perfect for multi-instrumentalists who know a section or song on more than one instrument. When viewing an instrument, you can see a list of all the songs and sections you know on that specific instrument.',
       video: InstrumentDemo
     },
     {
       title: 'Profile',
       subtitle: 'Uncover Trends In your own Tastes',
-      desc: 'SongBook keeps track of all the music you add and aggregates all that data to tell you more about your musical taste, as well as track your progress. Everything from how many songs you have added to your favorite artists can be found in your profile section of SongBook. Furthermore, every song imported from Spotify has the following audio metrics: Valence, Energy, Acousticness, Danceability, Instrumentalness, Speechiness and Loudness among others. SongBook tracks your preferences for those too to see what kind of music you prefer how to play.',
+      desc: 'SongBook keeps track of all the music you add and aggregates that data to tell you more about your musical tastes, as well as track your progress. Everything from how many songs you have added to a list of your favorite artists can be found in your profile section of SongBook. Furthermore, every song imported from Spotify has the following audio metrics: Valence, Energy, Acousticness, Danceability, Instrumentalness, Speechiness and Loudness among others. SongBook tracks your preferences for those too to see what kind of music you prefer how to play.',
       video: userMetricsDemo
     },
     {
       title: 'Deeper Integration with Spotify',
       subtitle: '(Premium members only)',
-      desc: 'If you have Spotify premium you can give SongBook access to your Spotify player. You can do this by hitting the “integrate spotify” button found on the filter drawer on the list pages or the manage account page. Once you do this you will see play buttons on every song or section you imported through Spotify. This is especially useful for playing along with songs, sections when learning covers.',
+      desc: 'If you have Spotify premium you can give SongBook access to your Spotify player. You can do this by hitting the “Integrate Spotify” button found on the filter drawer on the list pages or the manage account page. Once you do this you will see play buttons on every song or section you imported through Spotify. This is especially useful for playing along with songs and sections when learning covers.',
       video: IntegrateSpotifyDemo
     }
 
@@ -153,7 +153,11 @@ const Home = () => {
     return pngs.map((png, index) => {
       return (
           <Grid key={index} item xs={2} lg={1}>
-            <img onClick={() => setBlurbShow(index)} className={clsx(classes.graphic, { [classes.graphicActive]: blurbShow === index })} src={png} alt={`${png}`}/>
+            <img 
+                onClick={() => setBlurbShow(index)} 
+                className={clsx(classes.graphic, { [classes.graphicActive]: blurbShow === index })} 
+                src={png} 
+                alt={`${png}`}/>
           </Grid>
       )
     })
@@ -161,23 +165,23 @@ const Home = () => {
 
   return (
     <div className={classes.root}>
-        <Grid container justify="space-around" alignItems="center" className={classes.hero}>
-          <Grid item xs={10} md={6}>
-            <Typography variant="h2" className={classes.title} gutterBottom>Welcome To SongBook</Typography>
-            <Typography variant="subtitle1" gutterBottom className={classes.subtitle}>SongBook is a musical library web app for musicians to keep track of their song repertoire, instrument collection, and learning progress. </Typography>
-          </Grid>
-          <Grid item xs={10} md={5} style={{ textAlign: 'center' }}>
-            <Button className={classes.cta} onClick={() => {
+      <Grid container justify="space-around" alignItems="center" className={classes.hero}>
+        <Grid item xs={10} md={6}>
+          <Typography variant="h2" className={classes.title} gutterBottom>Welcome To SongBook</Typography>
+          <Typography variant="subtitle1" gutterBottom className={classes.subtitle}>SongBook is a musical library web app for musicians to organize their song repertoire, instrument collection, and learning progress. </Typography>
+        </Grid>
+        <Grid item xs={10} md={5} style={{ textAlign: 'center' }}>
+          <Button className={classes.cta} onClick={() => {
               user ? history.push('/search') : history.push('/register')
-            }}>
-               <span style={{ fontWeight: 600 }}>{ user ? 'Start Adding Songs Now' : 'Register Now'}</span>
-            </Button>
-          </Grid>
+          }}>
+              <span style={{ fontWeight: 600 }}>{ user ? 'Start Adding Songs Now' : 'Register Now'}</span>
+          </Button>
         </Grid>
-        <Grid style={{ margin: '2rem 0' }} container justify="space-evenly" alignItems="center">
-          {renderSvgs()}
-        </Grid>
-        <Blurb blurb={blurbs[blurbShow]}/>
+      </Grid>
+      <Grid style={{ margin: '2rem 0' }} container justify="space-evenly" alignItems="center">
+        {renderSvgs()}
+      </Grid>
+      <Blurb blurb={blurbs[blurbShow]}/>
     </div>
   )
 }
